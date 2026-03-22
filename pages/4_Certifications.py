21117e0bf7b9dd98337dfec1a98352d9ef515629
prodigy234 <k.gbenga234@gmail.com>
2026-03-22 10:15:23 +0100

    my fifth commit

--- pages/4_Certifications.py
+++ pages/4_Certifications.py
@@ -16,5 +16,5 @@ cols = [col1, col2, col3, col4, col5]
 
 for col, (img, title) in zip(cols, certs):
     with col:
-        st.image(f"assets/certificates/{img}", use_container_width=True)
+        st.image(f"assets/certificates/{img}", use_column_width=True)
         st.caption(title)
